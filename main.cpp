55e9a596baa484f89651739a8be6f6cca9295ba3
ondrej <okozinski@vsb.cz>
2024-01-26 12:54:42 +0100

    Second commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <fstream>
+void writeheader(std::ofstream file){
+};
+
+
+int main(int argc, char* argv[]){
+
+printf("Hi from RPI!\n");	
+
+
+}
